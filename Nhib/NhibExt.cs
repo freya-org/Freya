59fa2bba6d30b344f26135a8a090a89aaa1134f4
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-27 18:18:25 +0100

    Added NhibUtil

---
+++ Nhib/NhibExt.cs
@@ -0,0 +1,58 @@
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************/
+
+
+using FluentNHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Mapping;
+using FluentNHibernate.Mapping.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Freya.Nhib
+{
+    public static class NhibExt
+    {
+        public static FluentMappingsContainer AddFromNamespaceOf<T>(
+        this FluentMappingsContainer fmc)
+        {
+            string ns = typeof(T).Namespace;
+            IEnumerable<Type> types = typeof(T).Assembly.GetExportedTypes()
+                .Where(t => t.Namespace == ns)
+                .Where(x => IsMappingOf<IMappingProvider>(x) ||
+                            IsMappingOf<IIndeterminateSubclassMappingProvider>(x) ||
+                            IsMappingOf<IExternalComponentMappingProvider>(x) ||
+                            IsMappingOf<IFilterDefinition>(x));
+
+            foreach (Type t in types)
+            {
+                fmc.Add(t);
+            }
+
+            return fmc;
+        }
+
+        /// <summary>
+        /// Private helper method cribbed from FNH source (PersistenModel.cs:151)
+        /// </summary>
+        private static bool IsMappingOf<T>(Type type)
+        {
+            return !type.IsGenericType && typeof(T).IsAssignableFrom(type);
+        }
+    }
+}
