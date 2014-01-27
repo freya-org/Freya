99f37e42ed1e9b8d9cbf7d5cc7c333e7e2319c9c
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-27 17:41:52 +0100

    Added variant of contains for string

---
+++ Utils/StringExt.cs
@@ -0,0 +1,28 @@
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************/
+
+
+namespace Freya.Utils
+{
+    public static class StringExt
+    {
+        public static bool ContainsInvariant(this string sourceString, string filter)
+        {
+            return sourceString.ToLowerInvariant().Contains(filter);
+        }
+    }
+}
