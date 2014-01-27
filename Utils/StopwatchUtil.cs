1b1c7c6e6347f811886456e587862f4aedce4543
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-27 17:39:26 +0100

    Added util for measuring code

---
+++ Utils/StopwatchUtil.cs
@@ -0,0 +1,34 @@
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************/
+
+
+using System;
+using System.Diagnostics;
+
+namespace Freya.Utils
+{
+    public class StopwatchUtil
+    {
+        public static TimeSpan Time(Action action)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            action();
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+    }
+}
