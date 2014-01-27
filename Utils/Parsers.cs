eec9cc084bf971f3b867a466e74aac8bc79ef96b
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-27 17:40:24 +0100

    Added parsers from string to enum and conversely

---
+++ Utils/Parsers.cs
@@ -0,0 +1,40 @@
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************/
+
+
+using System;
+
+namespace Freya.Utils
+{
+    public static class Parser
+    {
+        public static String ToString(this Enum eff)
+        {
+            return Enum.GetName(eff.GetType(), eff);
+        }
+
+        public static EnumType ToEnum<EnumType>(this String enumValue)
+        {
+            return (EnumType)Enum.Parse(typeof(EnumType), enumValue);
+        }
+
+        public static EnumType ToEnum<EnumType>(this Enum eff)
+        {
+            return (EnumType)Enum.Parse(typeof(EnumType), ToString(eff));
+        }
+    }
+}
