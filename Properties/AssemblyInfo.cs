61bd36487f48279e09d0865f5439815840929201
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-27 17:39:44 +0100

    Added license file

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************
+ About Freya
+ -----------
+ Freya is an open source tool to build and run .NET applications. It is written in C# and
+ is compatible with the .NET platform. For more information visit our website.
+***********************************************************************************************/
+
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Freya")]
+[assembly: AssemblyDescription("Freya is an open source .NET 3.5 tool to build and run .NET applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Freya")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Freya")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d2d88123-261f-4658-b942-bca456b8dbdf")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
