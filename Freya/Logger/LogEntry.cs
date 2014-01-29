a4f79d6c6fb9c1aa7888c7d0d170be29e1d5800f
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-29 14:06:10 +0100

    Added properties of LogEntry

---
+++ Freya/Logger/LogEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Freya.Logger
+{
+    public class LogEntry
+    {
+        private string format;
+        private DateTime dateTime;
+        private BaseLogger.LogType logType;
+        private object value;
+
+        public LogEntry()
+        {
+            dateTime = DateTime.Now;
+        }
+
+        public string Format
+        {
+            get { return format; }
+            set { format = value; }
+        }
+
+        public DateTime DateTime
+        {
+            get { return dateTime; }
+        }
+
+        public BaseLogger.LogType LogType
+        {
+            get { return logType; }
+            set { logType = value; }
+        }
+
+        public object Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+    }
+}
