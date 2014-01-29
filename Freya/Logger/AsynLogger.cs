07fe01232019f630601943131770bb1614346242
Lukas Drahnik <L.Drahnik@gmail.com>
2014-01-29 16:00:33 +0100

    Added copyright

--- Freya/Logger/AsynLogger.cs
+++ Freya/Logger/AsynLogger.cs
@@ -1,6 +1,23 @@
-﻿using System.Collections.Generic;
+﻿/***********************************************************************************************
+ COPYRIGHT 2014 Drahník Lukáš
+ --------------------------
+
+ This file is part of Freya.
+ (Project Website: https://github.com/freya-org)
+
+ Freya is a free software. You can redistribute it and/or modify it under the terms of
+ the GNU General Public License as published by the Free Software Foundation, either version 3
+ of the License, or (at your option) any later version.
+
+ Freya is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ (See the GNU General Public License for more details: http://www.gnu.org/licenses/)
+
+ ***********************************************************************************************/
+
+
+using System.Collections.Generic;
 using System.IO;
-using Freya.Utils;
 using System.Threading;
 
 namespace Freya.Logger
