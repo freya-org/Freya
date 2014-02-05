829f789ce5fad9e15ee3f421a3b384ce903d2356
Lukas Drahnik <L.Drahnik@gmail.com>
2014-02-05 23:05:00 +0100

    Added extension to write name of variables

---
+++ Freya/FConsoleExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Freya
+{
+    public static class FConsoleExt
+    {
+        static void Print<T>(Expression<Func<T>> expression)
+        {
+            Console.WriteLine("{0}={1}", ((MemberExpression)expression.Body).Member.Name, expression.Compile()());
+        }
+    }
+}
