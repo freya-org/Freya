0dce54cb769860367f17a294ecabcdf1fc01d911
Lukas Drahnik <L.Drahnik@gmail.com>
2014-03-22 02:10:03 +0100

    Init BaseForm

---
+++ Freya/Forms/BaseForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace Freya.Forms
+{
+    public partial class BaseForm : Form
+    {
+
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
